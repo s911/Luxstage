579e8ba3a3660b5eaac8edd396bfdf36f9836280
s911 <luoxiaoshang@126.com>
2026-06-25 10:21:50 +0800

    update

---
+++ src/wp-content/themes/luxstage/page-about.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * About page alias template.
+ *
+ * Ensures sites using /about/ slug receive the same
+ * modern About Us layout as /about-us/.
+ */
+
+require get_stylesheet_directory() . '/page-about-us.php';
